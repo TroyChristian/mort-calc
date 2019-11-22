de89f991c85fc6668d6f78838d14657892db631c
Troy Christian <apostasy8@gmail.com>
2019-11-22 00:58:11 -0500

    Add basic persistence capabilites

--- mort-calc/mort-calc/Mortgage.swift
+++ mort-calc/mort-calc/Mortgage.swift
@@ -8,11 +8,20 @@
 
 import Foundation
 
-struct Mortgage{
+struct Mortgage: Codable{
     var loan:Int
     var term:Int
     var interestRate:Double
     var startDate:String
+    var endDate:String?
+    
+    init(loan:Int, term:Int, interestRate:Double, startDate:String, endDate:String = "1/1/2001"){
+        self.loan = loan
+        self.term = term
+        self.interestRate = interestRate
+        self.startDate = startDate
+        self.endDate = endDate 
+    }
    // var endDate:Date {
         //return startDate.advanced(by: //31536000 * term) // number //of seconds in a year times //the amount of years in the term.
     }
--- mort-calc/mort-calc/MortgageController.swift
+++ mort-calc/mort-calc/MortgageController.swift
@@ -10,16 +10,50 @@ import Foundation
 class MortgageController{
     var  mortgages:[Mortgage] = []
     func createMortgage(loan:Int, term:Int, interestRate:Double, startDate:String) -> Mortgage{
-        let newMortgage = Mortgage(loan: loan, term: term, interestRate: Double(interestRate), startDate: startDate)
+        let newMortgage = Mortgage(loan: loan, term: term, interestRate: Double(interestRate), startDate: startDate) //append new enddate when saved
         mortgages.append(newMortgage)
        return newMortgage
         
     }
     
+    private var mortgageListURL: URL? {
+        let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+        
+        let fileName = "mortgageList.plist"
+        
+        return documentDirectory?.appendingPathComponent(fileName)
+    }
+    
+    private func saveToPersistentStore() {
+        
+        let plistEncoder = PropertyListEncoder()
+        
+        do {
+            let notebooksData = try plistEncoder.encode(mortgages)
+            
+            guard let fileURL = mortgageListURL else { return }
+            
+            try notebooksData.write(to: fileURL)
+        } catch {
+            NSLog("Error encoding memories to property list: \(error)")
+        }
+    }
+    
+    private func loadFromPersistentStore() {
+        
+        do {
+            guard let fileURL = mortgageListURL else { return }
+            
+            let notebooksData = try Data(contentsOf: fileURL)
+            
+            let plistDecoder = PropertyListDecoder()
+            
+            self.mortgages = try plistDecoder.decode([Mortgage].self, from: notebooksData)
+        } catch {
+            NSLog("Error decoding memories from property list: \(error)")
+        }
+    }
+    
 }
 
 
-/*   var loan:Int
- var term:Int
- var interestRate:Int
- var startDate:String*/
